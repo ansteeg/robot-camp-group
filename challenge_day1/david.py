ea5b64965f48f5eb7b6347cb2067249b5bdbbf9b
Rumpelstilzli <david_wild@gmx.ch>
2024-09-02 12:40:50 +0200

    david paragraphs

--- challenge_day1/david.py
+++ challenge_day1/david.py
@@ -1,2 +1,16 @@
 def david():
     return "David"
+def david_p1():
+    return """
+    While we were walking back from the M1 building, nia will fall in the canal.
+    We were hourified that he just fell in the cannal.
+    """
+def david_p2():
+    return """ 
+    To cope with the horror Ava jupmed into the kanal out of solidarity. 
+    They couldn't swimm and was screaming so Ava tried there to rescue her. 
+    """
+def david_p3():
+    """
+    David was a bit stranged out by the situation, had a short luck at what was going on but then had continued with his dey.
+    """
