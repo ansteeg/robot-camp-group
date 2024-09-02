545d33a04ecdda1fa443e84a9a32b51bd773c223
Gabriella Beckley <gabriellabeckley@re-guest-195-169-168-37.wireless.uva.nl>
2024-09-02 12:17:22 +0100

    gab's paragraph

--- challenge_day1/gabriella.py
+++ challenge_day1/gabriella.py
@@ -1,2 +1,22 @@
 def gabriella():
     return "Gabriella"
+def gabriella_p1():
+    return """""
+
+    Gaby a girl born and raiced in the big City of New York she came to the realization she had out grown the loud 
+    and busy life. Luckly her friends from home Emma, Nia, David, Eve, Mark and Max were down to live life without 
+    any cabs or big building in sight.   
+    """""
+
+def gabriella_p2():
+    return """"
+
+    So the adventure begin when they all met up at the airport. As always Max and Mark were late, this time because they pulled an 
+    allnighter as they had another genious proyect idea that would safe the world. In the mean time, Nia, Emma, David and Eva went to
+    grab some food and played some kards. 
+    """
+
+def gabriella_p3():
+    return """""
+    Once all zeated on the plane, they were all excited to touch down in a new world outside the city. 
+    """""
