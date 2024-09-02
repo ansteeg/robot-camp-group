e4a06b2fca5eab9ede8ef78888b6f9dc2d7b57fd
Emma-Rose Blacher <emmaroseblacher@re-byods-146-50-156-150.wireless.uva.nl>
2024-09-02 13:03:30 +0200

    emma-rose's paragraphs

--- challenge_day1/emmarose.py
+++ challenge_day1/emmarose.py
@@ -1,2 +1,27 @@
 def emmarose():
     return "Emma-Rose"
+
+def emmarose_p1(): 
+    return """
+    At university, Emma, Max, mark, Gabriella; David, Ava, and Nia oftan gather in the campus café to discuss their classes. 
+    Emma and M@x usually sit together, with Emma doodling in her notebook while Max talks about his latest tech project. 
+    Mark and Gabriella sharing notes from their lectures, while David and Ava debate historical events. 
+    Nia, always organized, keeps track of their study schedules AND ensures everyone stays on task.
+    """
+
+def emmarose_p2(): 
+    return """
+    During group projects. Emma, Max, Mark, Gabriella, David, Ava, and Nia work seamlessly together. 
+    Emma’s creativity, Max’s technical skills, and Mark’s leadership help them tackle any challenge. 
+    Gabriella’s culinary treats keeps their energy up, while David’s knowledge and Ava’s musical interludes provide inspiration. 
+    Nia’s meticulous... planning ensures they meetall deadlines, making their projects a success.
+    """
+
+def emmarose_p3(): 
+    return """
+    In their free time, Emma, Max, Mark, Gabriella, David, Ava, and Nia enjoy exploring the campus. 
+    Emma and Ava often visit the art studio. while Max and Mark head to the gim, 
+    Gabriella & David can befound in the library, researching for fun, and Nia organizes weekend trips to nearby attractions. 
+    Their diverse interests and strong friendships make their university experience unforgettable.
+    """
+    
