c90b3909612a06fd6a4df1e6a5972df8b6853fee
Nia Espera <a5b6@riseup.net>
2024-09-02 12:52:13 +0200

    nia paragraphs

--- challenge_day1/nia.py
+++ challenge_day1/nia.py
@@ -1,2 +1,21 @@
 def nia():
     return "Nia"
+
+def nia_p1():
+    return """
+Nia and Ava retrunded to find David had decided to sacrifice Emma-Rose to whatever cult he's part of this time.
+Gabriella was understandably uspet at this, though Mark was nowhere to be found. Maybe he went off with Max again.
+    """
+
+def nia_p2():
+    return """
+It wasn't long before Maixx loudly delcared that he lost his wallet. Nia, of course, immediately suspected Emma-Rose
+as having stolen it. David didn't buy it, but Gabriella agredd and Mark was looking remarkably shifty.
+    """
+
+def nia_p3():
+    return """
+Nobody except Mark saw it coming. He forcefully shoved Gabriella and Emma-Rose out of the way, making sure to
+put in a snide comment about how if it werent'tr for him, Nia would have been struck also. David was genuinely
+fuming atthispoint; why would Max do this?
+    """
