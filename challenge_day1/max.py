bc6f46aec758ef211097bcb1eb942c7ffacb898a
m k <maxkoch3141@gmail.com>
2024-09-02 12:46:56 +0200

    Mark : Format for pagraphs

--- challenge_day1/max.py
+++ challenge_day1/max.py
@@ -1,2 +1,20 @@
 def max():
     return "Max"
+
+
+# Character name = your name
+# 3 paragraphs (1 for each act)
+# Each PARAGRAPH MUST have every character name
+# Make grammar mistakes
+# At least 2 commits from everyone
+# Every paragraph has to be fixed by 2 people
+
+def max_p1():
+    return ""
+
+def max_p2():
+    return "" 
+
+def max_p3():
+    return ""
+
