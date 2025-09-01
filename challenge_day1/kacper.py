396e378425611d1986d443a42e292e460de57f04
Ivar Huiskes <143857390+Nuceon@users.noreply.github.com>
2025-09-01 11:20:27 +0200

    Update kacper.py

--- challenge_day1/kacper.py
+++ challenge_day1/kacper.py
@@ -2,5 +2,5 @@ def name():
   return("kacper")
 
 def act3():
-  act3 = " Act III - The ridiculus relsoltion Just as it seemes bloom magasine would colapse underthe wieght of pastry crums and intsults, Margaret sttod, glitter pen and insult s, margaretn stood glitter pen in hand like sword, and proposed a duel: one haiku each. winner takes bloom. harold scribbledfuriously about fiscal responsibility ,greg wrote Bloom Bad seventten times and calleit minimalis genius, and Margaett produced an ode to croissant that accidentlay made everyoe cry.in the end nobody won, the meeting notes wereillegibele and bloom magazine survived another day simply because the janitoe locked the lights and went hoem."
+  act3 = " Act III – The Ridiculous Resolution Just as it seemed Bloom magazine would collapse under the weight of pastry crumbs and insults, Margaret stood, glitter pen in hand like a sword, and proposed a duel: One haiku each. Winner takes Bloom. Harold scribbled furiously about fiscal responsibility, Greg wrote Bloom bad seventeen times and called it minimalist genius, and Margaett produced an ode to croissant that accidentlay made everyoe cry.in the end nobody won, the meeting notes wereillegibele and bloom magazine survived another day simply because the janitoe locked the lights and went hoem."
   return act3
