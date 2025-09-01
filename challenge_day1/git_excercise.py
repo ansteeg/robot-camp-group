5caf51b0873d9db6b26b504d8e0500c8e60d6acd
Ivar Huiskes <143857390+Nuceon@users.noreply.github.com>
2025-09-01 11:14:57 +0200

    Update git_excercise.py

--- challenge_day1/git_excercise.py
+++ challenge_day1/git_excercise.py
@@ -1,9 +1,9 @@
 from Ivar import call_name
-from madeleine import get_name
+from madeleine import get_name, act1
 from Chris import call_name1
 from sophie import bestname
-from kacper import name
-from Todd import member_name
+from kacper import name, act3
+from Todd import member_name, act2
 
 print(f"This is Team zin er in. We are:")
 print(get_name())
@@ -14,3 +14,6 @@ print(call_name1())
 print(member_name())
 
 print("The story: /n")
+print(act1)
+print(act2)
+print(act3)
