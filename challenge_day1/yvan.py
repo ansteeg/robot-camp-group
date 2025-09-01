fb135ed10c19627d067abae2f82e385d529140bb
yvan-sumo <yvan@piccirilli.eu>
2025-09-01 12:52:05 +0200

    Update yvan.py

--- challenge_day1/yvan.py
+++ challenge_day1/yvan.py
@@ -1,2 +1,13 @@
-cldef yvan():
+def yvan():
     print("Yvan")
+
+
+def act1_scene4():
+    print("The Robaddies, featuring Don Quijote, Princess Dia, Din Jin Ping, Dracula, and Don Tour, are at luck! Their group also consists of a doctor, Doctor Nikki. Doctor Nikki is a plastic surgeon that really enjoys working on using plastic from the streets and putting it into people.")
+
+def act2_scene4():
+    print("Before becoming the Hero Party with Princess Dia, Dinh Jin Ping, Doctor Nikki, and Dracula, the brothers Quijote and Tour were only children. Their days were filled with chasing each other through the grass, swinging sticks like swords and dreaming of becoming knights like their father, Don John. Don John was a man of discipline and pride, respected by the townsfolk for his loyalty. To Quijote and Tour, he was larger than life, despite his awful drug addiction.. Their mother was gentle but firm. They thought the world would always be so simple: family, laughter, and the peaceful nightsky. None of them could imagine how quickly that dream would be torn apart.")
+
+
+def act3_scene4():
+    print("The decision came swiftly. With Jornadheim on the brink of collapse and Git’s armies amassing beyond the walls, the Robaddies could not afford hesitation. Don Quijote slammed his mug onto the table, declaring that the time for shadows and grief had passed. Princess Dia, her voice sharp, agreed that Git’s reign could only end in fire. Dinh Jin Ping nodded, stringing his bow, while Doctor Nikki scribbled plans of grotesque inventions on napkins. Dracula, smirking with fangs half-bared, promised to drench the battlefield in crimson. Don Tour, shaky but determined, clutched his brother’s shoulder, pledging to fight until his last breath. United, the six left the pub, not as wanderers, but as a single force marching toward war.")
