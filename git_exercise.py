88470f4d782c4ee5a0d74b84d5fc10ed8e532eba
lotjas <lottejaspers2@gmail.com>
2025-09-01 11:00:45 +0200

    Create git_exercise.py

---
+++ git_exercise.py
@@ -0,0 +1,9 @@
+from lotte_jaspers.py import who_am_i as lotte_who
+from member_bob import who_am_i as bob_who
+from member_sam import who_am_i as sam_who
+
+def introduce_team():
+    print("This is Team Brabus. We are:")
+    print(lotte_who())
+    print(bob_who())
+    print(sam_who())
