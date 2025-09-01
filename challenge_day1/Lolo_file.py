b49c6d87ed1ae1976d483ea3d69542b789908cc3
LorjinandLolo <131661447+LorjinandLolo@users.noreply.github.com>
2025-09-01 12:01:48 +0200

    Update to Lolo's file

--- challenge_day1/Lolo_file.py
+++ challenge_day1/Lolo_file.py
@@ -1,3 +1,12 @@
-def get_my_name():
+def name():
     return "lorijn" 
 
+character = 'Snob'
+
+
+def act1():
+    return "Snob adjusted his glasses nervously as he looked around at the unfamiliar surroundings. 'Are you sure this is the right way?' he asked Rob, Bob, Blob, Mob, and Gog, his voice tinged with uncertainty."
+def act2():
+    return "Blob carefully examined the piece of paer with the directions written on it, his brow furrowed in concentration. 'According to this, there is only one building',' he said, pointing to the surroundings 'But as you can see that is not the case.' Snob, Rob, Mob, Bob and Gog all sighed, feeling a bit lost but determined to find their way."
+def act3():
+    return "Mob let out a sigh of relief as they finally reached their destination. 'We made it,' she said, a smile spreading across her face. 'I couldn't have done it without you guys.' Gog noded in agreement, feeling a sense of acomplisment wash over him. Snob, Rob, Bob and Blob all smiled, feeling grateful for the support of their friends." 
