59857d3c466f18cf7bf5e175c898c89229541e72
lotjas <lottejaspers2@gmail.com>
2025-09-01 11:28:06 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -1,14 +1,12 @@
-import sys
-
-sys.path.append('/Users/lottejaspers/Documents/GitHub/computational-thinking-week-group-Brabus')
-
 from lotte_jaspers import who_am_i as lotte_who
 from Asjfaaq import who_am_i as asjfaaq_who
+from Juriaan_Rösingh.py
 
 def introduce_team():
     print("This is Team Brabus. We are:")
     print(lotte_who())
     print(asjfaaq_who())
+    print(juriaan_who())
 
 if __name__ == "__main__":
     introduce_team()
