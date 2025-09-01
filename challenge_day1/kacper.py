fc4f9ab5f93a0658ee350ecf89e0ca62413f73e2
kacperczajk <czajkoww@gmail.com>
2025-09-01 11:19:08 +0200

    Update kacper.py

--- challenge_day1/kacper.py
+++ challenge_day1/kacper.py
@@ -1,2 +1,6 @@
 def name():
   return("kacper")
+
+def act3():
+  act3 = " Act III - The ridiculus relsoltion Just as it seemes bloom magasine would colapse underthe wieght of pastry crums and intsults, Margaret sttod, glitter pen and insult s, margaretn stood glitter pen in hand like sword, and proposed a duel: one haiku each. winner takes bloom. harold scribbledfuriously about fiscal responsibility ,greg wrote Bloom Bad seventten times and calleit minimalis genius, and Margaett produced an ode to croissant that accidentlay made everyoe cry.in the end nobody won, the meeting notes wereillegibele and bloom magazine survived another day simply because the janitoe locked the lights and went hoem."
+  return act3
