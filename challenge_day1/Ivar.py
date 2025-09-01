d6a0a63a58d60832d8d4de70466d61a5d2f74197
Ivar Huiskes <143857390+Nuceon@users.noreply.github.com>
2025-09-01 10:35:04 +0200

    Create Ivar.py

---
+++ challenge_day1/Ivar.py
@@ -0,0 +1,3 @@
+def call_name():
+    print("Ivar")
+
---
+++ challenge_day1/git_excercise.py
@@ -0,0 +1,14 @@
+from Ivar import call_name
+from Madeleine import get_name
+from Chris import x
+from Sophie import bestname
+from Kacper import name
+from Todd import tname
+
+print(f"This is Team zin er in. We are:")
+get_name()
+bestname()
+call_name()
+name()
+x()
+tname()
