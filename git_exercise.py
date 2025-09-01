da639b7a13de938c8a35b36a6a9ec437c81e6f8e
asjfaaqr <asjfaaq.rs@gmail.com>
2025-09-01 11:03:46 +0200

    Update git_exercise.py

--- git_exercise.py
+++ git_exercise.py
@@ -5,5 +5,5 @@ from member_sam import who_am_i as sam_who
 def introduce_team():
     print("This is Team Brabus. We are:")
     print(lotte_who())
-    print(bob_who())
+    print(Asjfaaq_who())
     print(sam_who())
