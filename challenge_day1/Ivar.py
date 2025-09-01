604ad6f2982cd1c97ce7f4ac5853dbac9bf2e4f6
Ivar Huiskes <143857390+Nuceon@users.noreply.github.com>
2025-09-01 12:38:08 +0200

    Update Ivar.py

--- challenge_day1/Ivar.py
+++ challenge_day1/Ivar.py
@@ -1,2 +1,17 @@
 def call_name():
     print("Ivar")
+
+def act1_ivar:
+    act1 = """
+    """
+    return act1
+
+def act2_ivar():
+    act2 = """
+    """
+    return act2
+
+def act3_ivar():
+    act3= """
+    """
+    return act3
