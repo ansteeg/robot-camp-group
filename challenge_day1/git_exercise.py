303828052e9b59726c997de17a34d9dabc1ead35
Eleanor Luijkx <ecsluijkx@icloud.com>
2025-09-01 12:55:51 +0200

    Update git_exercise.py: add imprts

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,9 @@
-from diogo import diogo
-from kien import kien
-from ellie import ellie
-from yvan import yvan
-from nicolas import nicolas
-from vasil import vasil
+from diogo import diogo, act1_scene1, act2_scene1, act3_scene1
+from kien import kien, act1_scene5, act2_scene5, act3_scene5
+from ellie import ellie, act1_scene6, act2_scene6, act3_scene6
+from yvan import yvan, act1_scene4, act2_scene4, act3_scene4
+from nicolas import nicolas, act1_scene3, act2_scene3, act3_scene3
+from vasil import vasil, act1_scene2, act2_scene2, act3_scene2
 
 def group():
     print("Group Name: The Robaddies ")
