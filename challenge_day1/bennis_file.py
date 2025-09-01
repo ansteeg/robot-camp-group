588c037317a26d064cc5db57e1c30d69da6f5a12
emptxd <155116181+emptxd@users.noreply.github.com>
2025-09-01 11:42:17 +0200

    Update bennis_file.py

--- challenge_day1/bennis_file.py
+++ challenge_day1/bennis_file.py
@@ -4,10 +4,10 @@ def name():
 character = "Blob"
 
 def act1():
-    return "Blob stretched his arms wide, waving at Rob, Bob, Snob, Mob, and Gog. At last, the team was complete, and excitement sparkled in Blob’s voice as the sun dipped behind the hills."
+    return "Blob stretsched his arms wide, waving at Rob, Bob, Snob, Mob, and Gog. At last, the team was complete, and excitement sparkled in Blob’s voice as the sun dipped behind the hills."
 
 def act2():
-    return "Blob laughed loudly, trying to ease the tension as Rob, Bob, Snob, Mob, and Gog trudged through the dark forest. What’s a little mud compared to the adventure of a lifetime, Blob joked."
+    return "Blob laufed loudly, trying to ease the tension as Rob, Bob, Snob, Mob, and Gog trudged through the dark forest. What’s a little mud compared to the adventure of a lifetime, Blob joked."
 
 def act3():
-    return "Blob hugged Rob, Bob, Snob, Mob, and Gog. We did it, he exclaimed, tears of joy glistening in his eyes as they celebrated their hard-earned triumph."
+    return "Blob hugged Rob, Bob, Snob, Mob, and Gog. We did it, he exclaimed, tears of joy glistening in his eyes as they celebrayted their hard-earned triumph."
