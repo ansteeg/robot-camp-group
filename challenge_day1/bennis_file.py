323043e0fc01f4e8ad33c7a7bf7dfd6bfce3d656
emptxd <155116181+emptxd@users.noreply.github.com>
2025-09-01 11:39:56 +0200

    Update bennis_file.py

--- challenge_day1/bennis_file.py
+++ challenge_day1/bennis_file.py
@@ -1,2 +1,13 @@
 def name():
     return "Benni"
+
+character = "Blob"
+
+def act1():
+    return "Blob stretched his arms wide, waving at Rob, Bob, Snob, Mob, and Gog. At last, the team was complete, and excitement sparkled in Blob’s voice as the sun dipped behind the hills."
+
+def act2():
+    return "Blob laughed loudly, trying to ease the tension as Rob, Bob, Snob, Mob, and Gog trudged through the dark forest. What’s a little mud compared to the adventure of a lifetime, Blob joked."
+
+def act3():
+    return "Blob hugged Rob, Bob, Snob, Mob, and Gog. We did it, he exclaimed, tears of joy glistening in his eyes as they celebrated their hard-earned triumph."
