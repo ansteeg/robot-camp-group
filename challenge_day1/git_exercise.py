5cb62477425b6946d8a4accd95cd4dc8d77ccd40
Eleanor Luijkx <ecsluijkx@icloud.com>
2025-09-01 12:52:49 +0200

    Update git_exercise.py add story script

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -18,5 +18,46 @@ def group():
 
 group()
 
+from diogo import diogo
+from kien import kien
+from ellie import ellie
+from yvan import yvan
+from nicolas import nicolas
+from vasil import vasil
+
+def group():
+    print("Group Name: The Robaddies ")
+    print("Team Members:")
+
+    diogo()
+    kien()
+    ellie()
+    yvan()
+    nicolas()
+    vasil()
+
+group()
+
+def story():
+    act1_scene1()
+    act1_scene2()
+    act1_scene3()
+    act1_scene4()
+    act1_scene5()
+    act1_scene6()
+    act2_scene1()
+    act2_scene2()
+    act2_scene3()
+    act2_scene4()
+    act2_scene5()
+    act2_scene5()
+    act3_scene1()
+    act3_scene2()
+    act3_scene3()
+    act3_scene4()
+    act3_scene5()
+    act3_scene6()
+
+story()
 
 
