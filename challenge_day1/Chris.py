56749243068cfd2d7d19c0abbe43451359133f90
Chris Xia <chris.xia@student.uva.nl>
2025-09-01 12:43:17 +0200

    Finished 3 chapters of bonus story

--- challenge_day1/Chris.py
+++ challenge_day1/Chris.py
@@ -1,2 +1,22 @@
 def call_name1():
   return "Chris Xia"
+
+def act1_chris():
+  act1="""Margaret gasped so loudly that three pigeons outside the window startled and flew off. Harold, secretly 
+pleased that someone else was angry at Margaret, leaned back with a smirk and muttered, "Finally, someone 
+who speaks sense." The intern, unsure whether to take minutes or dial emergency services, quietly wrote "apocalypse???" in the meeting notes."""
+  return act1
+
+def act2_chris():
+  act2="""Instead, Harold squinted. “Is that… Comic Sans?” he whispered, scandalized. 
+  Margaret let out a strangled scream and tried to shield the others’ eyes. 
+  “We are professionals, Greg! Not savages!” she said, nearly choking again on residual croissant dust. Greg shrugged.
+  “It’s called aesthetic minimalism. Look it up.” He tried to sit on the table for effect but missed and nearly took out the complimentary fruit bowl."""
+  return act2
+
+def act3_chris():
+  act3="""In desperation, Harold suggested outsourcing content to fortune cookies. 
+  They immediately received a sample issue filled with predictions like “Your magazine will almost succeed today” 
+  and “Beware of raisins.” Readers loved it, confusing it for satire, and sales inexplicably spiked.
+  Greg began pitching Bloom as the “only publication printed entirely by destiny.”"""
+  return act3
