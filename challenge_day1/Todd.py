274ca1577699dbfe3ca55850f3595bb663ca8f41
Todd1424 <126423759+Todd1424@users.noreply.github.com>
2025-09-01 11:27:06 +0200

    Update Todd.py

--- challenge_day1/Todd.py
+++ challenge_day1/Todd.py
@@ -1,3 +1,10 @@
 def member_name():
     return "Todd"
-    
+act2 = """Act II – The Great Debate Greg pointed a trembeling finger at them and shouted, “Bloom is the downfal of civilizashun! 
+Your fonts are too whimsicel, your margens are anarcky, and your metaphors… un4givable!” 
+Margret gasped so dramatikally she inhailed an entier croisant crumb sideways, while Harrold clutched his spreedsheets like they were rosarry beeds. 
+“Sir, I’ll have you kno our margens are ISO certifide!” Harrold bellowed, his glassis fogging with rage. 
+Greg, not to be outdune, pulled out a crumpled peece of papper on wich he had drawn a stik figgur crying under the words ‘Bloom is doom’."""
+
+def ActII():
+    return act2
