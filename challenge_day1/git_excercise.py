9a80b2378e07e9269273c4fdb2a993bc2a68c91a
madeleine-hoffman <madeleine.hoffman@student.uva.nl>
2025-09-01 13:07:18 +0200

    Update git_excercise.py
    fixing sophie/chris syntax

--- challenge_day1/git_excercise.py
+++ challenge_day1/git_excercise.py
@@ -1,7 +1,7 @@
 from Ivar import call_name, act1_ivar, act2_ivar, act3_ivar
 from madeleine import get_name, act1_madeleine, act2_madeleine, act3_madeleine
 from Chris import call_name1, act1_chris, act2_chris, act3_chris
-from sophie import bestname, act1_chris, act2_chris, act3_chris
+from sophie import bestname, act1_sophie, act2_sophie, act3_sophie
 from kacper import name, act1_kacper, act2_kacper, act3_kacper
 from Todd import member_name, act1_todd, act2_todd, act3_todd
 
