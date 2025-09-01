8a9e4bb7f7b82755698bb9c623b54209e617ff8b
emptxd <155116181+emptxd@users.noreply.github.com>
2025-09-01 12:16:35 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,5 +1,3 @@
-# git_exercise.py
-
 import bennis_file
 import Lolo_file
 import marfa_file
@@ -7,10 +5,9 @@ import marie_file
 import paz_file
 import younes_file
 
-def print_team_roster():
-    print("This is Team Pinky Robots. We are:")
-
-    # Call each teammate's function
+def print_team_story():
+    # Team header
+    print("This is Team " + TEAM_NAME + ". We are:")
     print("-", bennis_file.name())
     print("-", Lolo_file.get_my_name())
     print("-", marfa_file.name())
@@ -18,5 +15,28 @@ def print_team_roster():
     print("-", paz_file.name())
     print("-", younes_file.name())
 
+    # The story (acts 1–3), rotating authors so nobody follows themselves
+    print(bennis_file.act1())
+    print(Lolo_file.act1())
+    print(marfa_file.act1())
+    print(marie_file.act1())
+    print(paz_file.act1())
+    print(younes_file.act1())
+
+    print(Lolo_file.act2())
+    print(bennis_file.act2())
+    print(marie_file.act2())
+    print(younes_file.act2())
+    print(paz_file.act2())
+    print(marfa_file.act2())
+
+    print(marie_file.act3())
+    print(bennis_file.act3())
+    print(younes_file.act3())
+    print(Lolo_file.act3())
+    print(marfa_file.act3())
+    print(paz_file.act3())
+
 if __name__ == "__main__":
-    print_team_roster()
+    TEAM_NAME = "YourTeamNameHere"
+    print_team_story()
