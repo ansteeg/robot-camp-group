cdee870ba934502e7a74cbdecdd240a7aea5c73a
marfakozeletsUvA <marfa.kozelets@student.uva.nl>
2025-09-02 13:27:04 +0200

    st 4 update comments

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,4 +1,3 @@
-# station4.py
 
 def solution_station_4(n):
     """
@@ -9,6 +8,6 @@ def solution_station_4(n):
         return False
 
     for i in range(2, n):
-        if n % i == 0:   # if divisible by i, not prime
+        if n % i == 0:  
             return False
     return True
