24634c790c554925500025702f0031bc0a2fe846
Kacper <czajkoww@gmail.com>
2025-09-02 10:06:08 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,3 +1,17 @@
 def solution_station_7(int):
+
+
+  """ 
+  eval()
+  e*a*d = 10.5
+  d+c = 11
+  b*a = -3
+  e*b*d*a = -10.5
+  e+b = -0.5
+  e+d+a+b = 9.5
+  a+d+b+c = 13
+  a*d+e+b = 20.5
+  e*b+d = 6.5
   
+  """
   
