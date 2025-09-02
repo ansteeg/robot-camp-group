ef35abb8c93806c88669a9ff41cd9d20728b5a2f
lotjas <lottejaspers2@gmail.com>
2025-09-02 10:42:39 +0200

    Update station3.py

--- challenge_day2/tests/station3.py
+++ challenge_day2/tests/station3.py
@@ -1,20 +1,4 @@
-33	32704
-True	?
-58	17371
-False	
-23	11744
-False	
-22	58547
-False	
-7	99991
-False	
-
-def solution_station_3(n: int) -> bool:
-    if n < 2:
-        return False
-    if n % 2 == 0 and n != 2:
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
+def solution_station_3(b: int) -> bool:
+# Return True if the sum of digits of b is divisible by 3, else False.
+    digit_sum = sum(int(d) for d in str(b))
+    return digit_sum % 3 == 0
