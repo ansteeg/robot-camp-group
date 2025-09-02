a2a9f1274f80125bb709172fbf030c67d36b3d55
sophiepronk <148351152+sophiepronk@users.noreply.github.com>
2025-09-02 09:55:45 +0200

    Update station1.py

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,12 +1,15 @@
 def solution_station_1(n):
-  if n <= 0:
-    return 0
-  elif n == 1:
-    return 1
-  else:
-    return solution_station_1(n-1) + solution_station_1(n-2)
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n+1):
+        a, b = b, a+b
+    return b
 
-print (solution_station_1(69))
+print(solution_station_1(69))
   
 # italian mathematician (fibonacci sequence)
 # 0 0
