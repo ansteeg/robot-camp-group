41f846e3f97d9cb64f6b2cc7493bfdc2bb1e4415
sophiepronk <148351152+sophiepronk@users.noreply.github.com>
2025-09-02 09:31:56 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,4 +1,11 @@
 def solution_station_5(input):
-""" input an integer, """
+  ('9:25:30', "Maria", 3, "Jelle", "")
+  ('9:25:20', "Mies", 3, "Nadee", "")
+  ('9:25:15', "Philip", 4, "Maciej", "")
+
+
+# Format: (time: str, station1_input: int, station2_input: str, station3_input: int, station4_input: int, station5_input: str, station6_input: int, station7_input: str)
+# Example: ('12:30:00', 1, '1990-01-01', 2, 3, "John", 4, "e=mc^2")
+observation1 = ...
 
   return output
