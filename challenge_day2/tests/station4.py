80f07334fb7c2a7a4a1a62b01151086d966e95d7
lotjas <lottejaspers2@gmail.com>
2025-09-02 10:39:52 +0200

    Update station4.py

--- challenge_day2/tests/station4.py
+++ challenge_day2/tests/station4.py
@@ -1,7 +1,5 @@
 def solution_station_4(b: int) -> bool:
-    """
-    Return True if b is a prime number, False otherwise.
-    """
+# Return True if b is a prime number, False otherwise.
     if b < 2:
         return False
     for i in range(2, int(b**0.5) + 1):
