05958c88ba36473f191a15391570afb717076998
lotjas <lottejaspers2@gmail.com>
2025-09-02 09:47:05 +0200

    Update station3.py

--- challenge_day2/tests/station3.py
+++ challenge_day2/tests/station3.py
@@ -1,1 +1,10 @@
-
+def solution_station_3(n: int) -> bool:
+    # True als n een priemgetal is
+    if n < 2:
+        return False
+    if n % 2 == 0 and n != 2:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
