e5ded542dc09628974ac4c93f32a34c32661ea79
Kacper <czajkoww@gmail.com>
2025-09-02 10:15:09 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,5 +1,5 @@
 def solution_station_7(expression):
-  eval(expression, globals= [a = 3,  b = -1,  c = 4,  d = 7,  e = 0.5], locals=None)
+  eval(expression, globals= {'a': 3, 'b': -1, 'c': 4, 'd': 7, 'e': 0.5}, locals=None)
   
   return expression
 
