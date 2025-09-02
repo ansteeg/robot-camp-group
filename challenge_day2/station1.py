4884e5e64943d760e27c428fd7ef3e65eeee53c5
tuntuntun339 <vassil.konstantin@gmail.com>
2025-09-02 12:26:46 +0200

    added a function

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,21 +1,17 @@
-from functools import lru_cache
-
-def _fib_fast_doubling(n: int):
-
-    if n == 0:
-        return (0, 1)
-    a, b = _fib_fast_doubling(n >> 1)
-    c = a * ((b << 1) - a)
-    d = a*a + b*b
-    if n & 1:
-        return (d, c + d)
-    else:
-        return (c, d)
-
-def fib(n: int) -> int:
-    if n < 0:
-        raise ValueError("n must be nonnegative")
-    return _fib_fast_doubling(n)[0]
-
 def solution_station_1():
+    def fib(n: int) -> int:
+        def fast_doubling(n: int):
+            if n == 0:
+                return (0, 1)
+            a, b = fast_doubling(n >> 1)
+            c = a * ((b << 1) - a)
+            d = a*a + b*b
+            if n & 1:
+                return (d, c + d)
+            else:
+                return (c, d)
+        if n < 0:
+            raise ValueError("n must be nonnegative")
+        return fast_doubling(n)[0]
+
     return fib(3)
