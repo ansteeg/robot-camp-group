4a86ca2dd28f06c28e4db28294be20f2f68caa62
madeleine-hoffman <madeleine.hoffman@student.uva.nl>
2025-09-02 10:22:37 +0200

    Update station4.py
    fixing syntax

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-def is_prime(n):
-  if n < 2:
-    return False
-  if n == 2:   
-    return True
-  if n % 2 == 0:  
-    return False
+def solution_station_4(n):
+    if n < 2:
+        return False
+    if n == 2:   
+        return True
+    if n % 2 == 0:  
+        return False
     
+    for i in range(3, int(np.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-for i in range(3, int(np.sqrt(n)) + 1, 2):
-  if n % i == 0:
-    return False
-  
-return True
-  
+print(solution_station_4(2))   # True
+print(solution_station_4(9))   # False
+print(solution_station_4(17))  # True
