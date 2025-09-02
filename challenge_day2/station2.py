181be7b8a2b10f09f7e7cc82fb4c699992c2ae32
ShiKen08 <sykien08@gmail.com>
2025-09-02 10:40:40 +0200

    Update station2.py

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,1 +1,13 @@
+from datetime import datetime, timedelta
+
+def solution_station_2(date2):
+    dt2 = datetime.strptime(date2, "%Y-%m-%d")
+    japanese_weekdays = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
+    return japanese_weekdays[dt2.weekday()]
+
+
+print(solution_station_2("2024-01-01"))  # 火曜日
+print(solution_station_2("2024-07-20"))  # 土曜日
+
+
 
