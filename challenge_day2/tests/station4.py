7a153b6484975a942fd96b72fedc641c18bf7f51
lotjas <lottejaspers2@gmail.com>
2025-09-02 10:03:04 +0200

    Update station4.py

--- challenge_day2/tests/station4.py
+++ challenge_day2/tests/station4.py
@@ -1,1 +1,6 @@
-
+def solution_station_4(a: int, b: int) -> bool:
+    if b % a == 0:
+        return True
+    if b > 1 and all(b % i for i in range(2, int(b**0.5) + 1)):
+        return True
+    return False
