0c732fc6ee12add79aeccb38afd2e6d21d747113
ShiKen08 <sykien08@gmail.com>
2025-09-03 13:03:08 +0200

    Update charactername.py

--- challenge_day1/charactername.py
+++ challenge_day1/charactername.py
@@ -1,1 +1,17 @@
+def D():
+  print "Don Quijote" 
 
+def P():
+  print "Princess Dia" 
+
+def J():
+  print "Dinh Jin Ping" 
+
+def N():
+  print "Doctor Nikki" 
+
+def A():
+  print "Dracula" 
+
+def T():
+  print "Don Tour
