7650ac4f3b8f1d653451da13106efd0164568d22
ShiKen08 <sykien08@gmail.com>
2025-09-03 22:55:38 +0200

    Update ellie.py

--- challenge_day1/ellie.py
+++ challenge_day1/ellie.py
@@ -3,7 +3,7 @@ def ellie():
     print ("Ellie")
 
 def act1_scene6():
-    print(f"The members {P()}, {J()}, {N()}, {A()}, and {A()} look up to the brothers {D()} and {T()} as their leaders. {T()}, {D()}’s younger brother, considers himself as a power ranger. The blue one. He takes inspiration from him. He has everything to resemble him and act like him, amid his heavy drug addiction.")
+    print(f"The members {P()}, {J()}, {N()}, and {A()} look up to the brothers {D()} and {T()} as their leaders. {T()}, {D()}’s younger brother, considers himself as a power ranger. The blue one. He takes inspiration from him. He has everything to resemble him and act like him, amid his heavy drug addiction.")
 
 def act2_scene6():
     print(f"In a single night, their peaceful lives were changed forever. {D()}, traumatized by his helplessness, craved strength above all else, in order to protect the people he loves. {T()}, the younger brother, was more unlucky, unable to overcome their parents’ death. While Quijote trained, {T()} strayed, hiding from the weight of loss behind false courage and the poisons that dulled his pain. Yet the tragedy of Kallin was not theirs alone. {P()} too had lost her parents to Git, her throne turned into a burden she could not yet bear. {J()}, who dreamed of traveling the land, would find no choice but to raise his hand in battle. {N()}, in his strange obsession with rebuilding broken things, saw only more shattered lives to mend. And {A()}, born of shadow, was drawn to the river of blood spilling out. Each carried scars of their own, but together, these wounds would bind them. This was the beginning of the Robaddies, forged not in glory, but in fire, grief, and the promise of vengeance against Git.")
