4d4f6fd2c0fea1a886938278f10f0d557df2e1e2
madeleine-hoffman <madeleine.hoffman@student.uva.nl>
2025-09-04 10:37:30 +0200

    Update Chris.py

--- challenge_day1/Chris.py
+++ challenge_day1/Chris.py
@@ -1,7 +1,7 @@
 def call_name1():
   return "Chris Xia"
 
-def act1_chris():
+def act1_55():
   act1="""Margaret gasped so loudly that three pigeons outside the window startled and flew off. Harold, secretly 
   pleased that someone else was angry at Margaret, leaned back with a smirk and muttered, "Finally, someone 
   who speaks sense." The intern, unsure whether to take minutes or dial emergency services, quietly wrote "apocalypse???" in the meeting notes. 
@@ -9,7 +9,7 @@ def act1_chris():
   Margaret and Harold pretended not to notice."""
   return act1
 
-def act2_chris():
+def act2_55():
   act2="""Instead, Harold squinted. “Is that… Comic Sans?” he whispered, scandalized. 
   Margaret let out a strangled scream and tried to shield the others’ eyes. 
   “We are professionals, Greg! Not savages!” she said, nearly choking again on residual croissant dust. Greg shrugged.
@@ -17,7 +17,7 @@ def act2_chris():
   The intern scribbled it all down as the janitor hovered by the light switch, while Margaret, Harold, and Greg argued about fonts."""
   return act2
 
-def act3_chris():
+def act3_55():
   act3="""In desperation, Harold suggested outsourcing content to fortune cookies. 
   They immediately received a sample issue filled with predictions like “Your magazine will almost succeed today” 
   and “Beware of raisins.” Readers loved it, confusing it for satire, and sales inexplicably spiked.
@@ -25,3 +25,4 @@ def act3_chris():
   Margaret called it visionary, Harold claimed it was efficient, Greg said it was destiny, 
   the intern nodded gravely, and even the janitor agreed it beat sweeping glitter.”"""
   return act3
+
