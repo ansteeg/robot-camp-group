ccc26c877248539e26e73c49943aa57a17592619
ShiKen08 <sykien08@gmail.com>
2025-09-04 09:35:26 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -18,25 +18,6 @@ def group():
 
 group()
 
-from diogo import diogo
-from kien import kien
-from ellie import ellie
-from yvan import yvan
-from nicolas import nicolas
-from vasil import vasil
-
-def group():
-    print("Group Name: The Robaddies ")
-    print("Team Members:")
-
-    diogo()
-    kien()
-    ellie()
-    yvan()
-    nicolas()
-    vasil()
-
-group()
 
 def story():
     act1_scene1()
