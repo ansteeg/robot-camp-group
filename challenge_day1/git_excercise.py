1702545379a52f7ca45cc4d58b130e39aeaea232
madeleine-hoffman <madeleine.hoffman@student.uva.nl>
2025-09-04 10:34:58 +0200

    fixing git_excercise.py

--- challenge_day1/git_excercise.py
+++ challenge_day1/git_excercise.py
@@ -1,23 +1,9 @@
-from Ivar import call_name
-from Madeleine import get_name
-from Chris import x
-from Sophie import bestname
-from Kacper import name
-from Todd import tname
-
-print(f"This is Team zin er in. We are:")
-get_name()
-bestname()
-call_name()
-name()
-x()
-tname()
-from Ivar import call_name, act1_ivar, act2_ivar, act3_ivar
-from madeleine import get_teamname, get_name, act1_madeleine, act2_madeleine, act3_madeleine
-from Chris import call_name1, act1_chris, act2_chris, act3_chris
-from sophie import bestname, act1_sophie, act2_sophie, act3_sophie
-from kacper import name, act1_kacper, act2_kacper, act3_kacper
-from Todd import member_name, act1_todd, act2_todd, act3_todd
+from Ivar import call_name, act1_33, act2_33, act3_33
+from madeleine import get_teamname, get_name, act1_11, act2_11, act3_11
+from Chris import call_name1, act1_55, act2_55, act3_55
+from sophie import bestname, act1_22, act2_22, act3_22
+from kacper import name, act1_44, act2_44, act3_44
+from Todd import member_name, act1_66, act2_66, act3_66
 
 print(get_teamname())
 print(get_name())
@@ -29,7 +15,7 @@ print(member_name())
 
 print("The story:")
 for n in range(1, 4): 
-    for person in ["madeleine", "sophie", "ivar", "kacper", "chris", "todd"]:
+    for person in [11, 22, 33, 44, 55, 66]:
         func_name = f"act{n}_{person}"
         func = globals()[func_name]
         print(func())
