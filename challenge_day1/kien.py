eb97a5c367a683820d4104c329c043e92c376928
Diogo Baptista <diogobaptista@re-byods-146-50-152-63.wireless.uva.nl>
2025-09-04 10:39:39 +0200

    lazy function

--- challenge_day1/kien.py
+++ challenge_day1/kien.py
@@ -2,30 +2,29 @@
 def kien_name():
   print("Kien")
 
-def D():
-    return "Don Quijote"
-
-def P():
-    return "Princess Dia"
-
 def J():
     return "Dinh Jin Ping"
 
-def N():
-    return "Doctor Nikki"
-
-def A():
-    return "Dracula"
-
-def T():
-    return "Don Tour"
-
-
 def act1_scene5():
+    from diogo import D
+    from ellie import P
+    from nicolas import N
+    from vasil import A
+    from yvan import T
     print(f"{A()}, the scariest member of the Robaddies, often scares the other members, {D()}, {P()}, {J()}, {N()}, and {T()}. {A()} is a hero who hides within the shadows, wielding ancient powers as he fights alongside his teammates. His eerie presence often sends shivers through his friends and enemies.")
 
 def act2_scene5():
+    from diogo import D
+    from ellie import P
+    from nicolas import N
+    from vasil import A
+    from yvan import T
     print(f"The attack on Kalli came like lightning in the night, sudden and merciless. Arrows rained from the heavens, homes erupted in fire, and the cries of the innocent filled the streets. The peaceful time on Kallin stood no chance, the army of the night was running all over town, chaos erupted everywhere. The chief guard, Don John, father to {D()} and {T()}, can only stood still in front of his house in the hope of protecting his family. Sword drawn, he slashes down countless of enemies, in the town that is engulfed by flames. Hearing the desperate cries of the citizen, unable to contain himself Don John rushes into the hellish battle to help. Seeing her husband rush into battle, Daniela, their mom, can only rush the boys safety in the hope that they will survive. Being rushed by the unsurmountable of Git soldiers, their mother can only stay behind as bait in order for her boys to survive. This event is the major gear that forms the Robaddies party of {D()}, {P()}, {J()}, {N()}, {A()}, and {A()}.")
 
 def act3_scene5():
+    from diogo import D
+    from ellie import P
+    from nicolas import N
+    from vasil import A
+    from yvan import T
     print(f"The plain outside Jornadheim erupted into chaos as the Robaddies clashed with the Git. {D()}’s blade gleamed in the torchlight, cleaving through enemy ranks with fury born of a man revenging his father’s death. {T()} fought at his side, bottle in hand, striking with the astounding courage of a man who is completely blacking out. {P()} unleashed her rage, her war cries echoing across the valley, while {J()}’s arrows whistled through the air, felling Git soldiers before they could draw breath. {N()} unleashed twisted contraptions of bone and steel, horrifying both ally and foe. {A()}, descended in his true form. Thousands of bats swarmed the battlefield, tearing through flesh and bones. Together, the Robaddies seemed unstoppable, yet the enemy was endless.")
