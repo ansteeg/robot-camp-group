a1a8a989345c4a2cfd7cff2c52b761c963f749c3
madeleine-hoffman <madeleine.hoffman@student.uva.nl>
2025-09-04 10:36:38 +0200

    Update Ivar.py

--- challenge_day1/Ivar.py
+++ challenge_day1/Ivar.py
@@ -1,7 +1,7 @@
 def call_name():
     print("Ivar")
 
-def act1_ivar():
+def act1_33():
     act1 = """ Before she could launch into her ten-minute PowerPoint on "The Socioeconomic Importance of Glitter," 
 the door creaked open. In stormed Greg, the infamous Bloom hater, known in town for once trying to 
 sue a sunflower for copyright infringement. His scarf was dramatically long, his expression permanently 
@@ -9,17 +9,17 @@ scowling, and his hands clutched a binder labeled "Bloom = Doom."
     """
     return act1
 
-def act2_ivar():
+def act2_33():
     act2 = """
     Meanwhile, Harold clutched his meticulously organized spreadsheets as if
     they were sacred texts. His left eye twitched. “Sir,” he began, adjusting his glasses with righteous fury,
-    “I'll have you know our margins are ISO certified!” The word “ISO” echoed like a war cry through the 
+    “I’ll have you know our margins are ISO certified!” The word “ISO” echoed like a war cry through the 
     conference room. His glasses began to fog, equal parts rage and leftover coffee steam. He rose halfway 
-    out of his chair before realizing he wasn't sure if his belt would hold under pressure, and promptly sat back down with dignity barely intact.
+    out of his chair before realizing he wasn’t sure if his belt would hold under pressure, and promptly sat back down with dignity barely intact.
     """
     return act2
 
-def act3_ivar():
+def act3_33():
     act3= """
     Meanwhile, Margaret secretly hired a balloon artist to “elevate the brand.” Soon the office was crammed 
     with balloon giraffes, balloon baguettes, and an unsettling balloon effigy of Harold holding a calculator. 
@@ -27,4 +27,5 @@ def act3_ivar():
     poet while trapped between two balloon flamingos, and the transcript consisted entirely of “honk squeak thud.”
     """
     return act3
+
 
