243bb8bff19c9960d0021e8f81a37468a944967f
Eleanor Luijkx <ecsluijkx@icloud.com>
2025-09-04 10:00:35 +0200

    Update yvan.py: name fix

--- challenge_day1/yvan.py
+++ challenge_day1/yvan.py
@@ -1,14 +1,20 @@
-from charactername import D, P, J, N, A, T
+from diogo.py import D
+from ellie.py import P
+from kien.py import J
+from nicolas.py import N
+from vasil.py import A
+
 def yvan():
     print("Yvan")
 
+def T():
+  return "Don Tour"
 
 def act1_scene4():
     print(f"The Robaddies, featuring {D()}, {P()}, {J()}, {N()}, {A()}, and {T()}, are at luck! Their group also consists of a doctor, {N()}. {N()} is a plastic surgeon that really enjoys working on using plastic from the streets and putting it into people.")
 
 def act2_scene4():
     print(f"Before becoming the Hero Party with {P()}, {J()}, {N()}, and {A()}, the brothers {D()} and {T()} were only children. Their days were filled with chasing each other through the grass, swinging sticks like swords and dreaming of becoming knights like their father, Don John. Don John was a man of discipline and pride, respected by the townsfolk for his loyalty. To {D()} and {T()}, he was larger than life, despite his awful drug addiction.. Their mother was gentle but firm. They thought the world would always be so simple: family, laughter, and the peaceful nightsky. None of them could imagine how quickly that dream would be torn apart.")
-
 
 def act3_scene4():
     print(f"The decision came swiftly. With Jornadheim on the brink of collapse and Git’s armies amassing beyond the walls, the Robaddies could not afford hesitation. {D()} slammed his mug onto the table, declaring that the time for shadows and grief had passed. {P()}, her voice sharp, agreed that Git’s reign could only end in fire. {J()} nodded, stringing his bow, while {N()} scribbled plans of grotesque inventions on napkins. {A()}, smirking with fangs half-bared, promised to drench the battlefield in crimson. {T()}, shaky but determined, clutched his brother’s shoulder, pledging to fight until his last breath. United, the six left the pub, not as wanderers, but as a single force marching toward war.")
